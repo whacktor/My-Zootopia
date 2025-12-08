a7581c83a92c5440aa359d60e81b963b340d4e42
whacktor <lammelviktor2@gmail.com>
2025-12-08 16:29:26 +0100

    Main-Function added

--- Projektordner/animal_web_generator.py
+++ Projektordner/animal_web_generator.py
@@ -1,1 +1,36 @@
-#erster commit
+import json
+
+def load_data(file_path):
+    """Loads a JSON file."""
+    with open(file_path, "r") as handle:
+        return json.load(handle)
+
+
+animals_data = load_data("animals_data.json")
+
+
+for animal in animals_data:
+    name = animal.get("name")
+
+
+    characteristics = animal.get("characteristics", {})
+    diet = characteristics.get("diet")
+    animal_type = characteristics.get("type")
+
+
+    locations = animal.get("locations", [])
+
+
+    if name:
+        print(f"Name: {name}")
+
+    if diet:
+        print(f"Diet: {diet}")
+
+    if locations:
+        print(f"Location: {locations[0]}")
+
+    if animal_type:
+        print(f"Type: {animal_type}")
+
+    print()
